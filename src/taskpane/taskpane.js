2d3c0b60f042d86689d96546a0b52f3361f9151d
samba914 <sambadiarra2000@gmail.com>
2021-09-15 11:22:56 +0200

    initial commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+// images references in the manifest
+import "../../assets/icon-16.png";
+import "../../assets/icon-32.png";
+import "../../assets/icon-80.png";
+
+/* global document, Office, Word */
+
+Office.onReady((info) => {
+  document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    var code = event.code;
+    document.getElementById("run").innerHTML="ok";
+    // Alert the key name and key code on keydown
+    ;
+  }, false);
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+    
+  }
+});
+
+export async function run() {
+  return Word.run(async (context) => {
+    document.getElementById("run").innerHTML="te";
+    /**
+     * Insert your Word code here
+     */
+
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+    // change the paragraph color to blue.
+    paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
